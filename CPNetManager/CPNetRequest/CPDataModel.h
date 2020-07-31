9486c6ae05b66aad5c3af00e5bdb3e0fd0f3bf8d
cp271007323 <271007323@qq.com>
2020-07-31 18:12:40 +0800

    1
    1

--- CPNetManager/CPNetRequest/CPDataModel.h
+++ CPNetManager/CPNetRequest/CPDataModel.h
@@ -12,6 +12,8 @@
 //文件上传类型
 typedef enum : NSUInteger {
     CPDataModelUpload_Image
+    CPDataModelUpload_Video,
+    CPDataModelUpload_Audio,
 } CPDataModelUpload_Type;
 
 @interface CPDataModel : NSObject
@@ -31,5 +33,31 @@ typedef enum : NSUInteger {
 - (void)setDataModelWithImage:(UIImage * _Nonnull)image
                          type:(CPDataModelUpload_Type)type
                      fileName:(NSString * _Nonnull)fileName;
+
+
+//视频
+@property(assign, nonatomic)    CPDataModelUpload_Type   video_type;
+@property(assign, nonatomic)    CPDataModelUpload_Type   video_Thumb_type;
+@property(strong, nonatomic)    NSString                * _Null_unspecified video_path;
+@property(strong, nonatomic)    NSData                  * _Null_unspecified video_data;
+@property(strong, nonatomic)    NSData                  * _Null_unspecified video_thumb_data;
+@property(copy, nonatomic)      NSString                * _Null_unspecified video_Name;
+@property(copy, nonatomic)      NSString                * _Null_unspecified video_thumb_name;
+@property(copy, nonatomic)      NSString                * _Null_unspecified video_fileName;
+@property(copy, nonatomic)      NSString                * _Null_unspecified video_thumb_fileName;
+@property(copy, nonatomic)      NSString                * _Null_unspecified video_mimeType;
+@property(copy, nonatomic)      NSString                * _Null_unspecified video_thumb_mimeType;
+
+-(void)setDataModelWithVideoPath:(NSString * _Nonnull)videoPath type:(CPDataModelUpload_Type)type fileName:(NSString * _Nonnull)fileName;
+
+//语音
+@property(assign, nonatomic)    CPDataModelUpload_Type   audio_type;
+@property(strong, nonatomic)    NSString                * _Null_unspecified audio_path;
+@property(strong, nonatomic)    NSData                  * _Null_unspecified audio_data;
+@property(copy, nonatomic)      NSString                * _Null_unspecified audio_name;
+@property(copy, nonatomic)      NSString                * _Null_unspecified audio_fileName;
+@property(copy, nonatomic)      NSString                * _Null_unspecified audio_mimeType;
+
+-(void)setDataModelWithAudioPath:(NSString * _Nonnull)AudioPath type:(CPDataModelUpload_Type)type fileName:(NSString * _Nonnull)fileName;
 
 @end
--- CPNetManager/CPNetRequest/CPDataModel.m
+++ CPNetManager/CPNetRequest/CPDataModel.m
@@ -10,6 +10,9 @@
 #import <AVFoundation/AVFoundation.h>
 
 static NSString *upload_Image = @"multipart/form-data";
+static NSString *upload_Audio = @"amr";
+static NSString *upload_Video = @"mp4";
+
 
 #define CPDataModelTimeIntervalSince1970 [NSString stringWithFormat:@"%lld",(long long)([[NSDate date] timeIntervalSince1970] * 1000)]
 
@@ -18,9 +21,12 @@ static NSString *upload_Image = @"multipart/form-data";
 - (void)setType:(CPDataModelUpload_Type)type
 {
     _type = type;
-    if (type == CPDataModelUpload_Image)
-        {
+    if (type == CPDataModelUpload_Image) {
         self.image_type = _type;
+    }else if (type == CPDataModelUpload_Video){
+        self.video_type = _type;
+    }else if (type == CPDataModelUpload_Audio){
+        self.audio_type = _type;
     }
 }
 
@@ -28,6 +34,17 @@ static NSString *upload_Image = @"multipart/form-data";
 {
     _image_type = image_type;
     self.image_mimeType = upload_Image;
+}
+
+-(void)setVideo_type:(CPDataModelUpload_Type)video_type{
+    _video_type = video_type;
+    self.video_mimeType = upload_Video;
+    self.video_thumb_mimeType = upload_Image;
+}
+
+-(void)setAudio_type:(CPDataModelUpload_Type)audio_type{
+    _audio_type = audio_type;
+    self.audio_mimeType = upload_Audio;
 }
 
 - (void)setDataModelWithImage:(UIImage * _Nonnull)image
@@ -43,6 +60,43 @@ static NSString *upload_Image = @"multipart/form-data";
     NSString *timeStr = CPDataModelTimeIntervalSince1970;
     self.image_name = [NSString stringWithFormat:@"%@",fileName];
     self.image_fileName = [NSString stringWithFormat:@"%@_%@.png",fileName,timeStr];
+}
+
+-(void)setDataModelWithVideoPath:(NSString * _Nonnull)videoPath
+                            type:(CPDataModelUpload_Type)type
+                        fileName:(NSString * _Nonnull)fileName{
+    self.type = type;
+    self.video_path = videoPath;
+    self.video_data = [NSData dataWithContentsOfFile:videoPath];
+    self.video_thumb_data = UIImageJPEGRepresentation([self videoToImg:[NSURL fileURLWithPath:videoPath]], .7);
+    NSString *timeStr = CPDataModelTimeIntervalSince1970;
+    self.video_Name = [NSString stringWithFormat:@"%@_%@",fileName,timeStr];
+    self.video_thumb_name = [NSString stringWithFormat:@"%@_%@_s",fileName,timeStr];
+    self.video_fileName = [NSString stringWithFormat:@"%@_%@.mp4",fileName,timeStr];
+    self.video_thumb_fileName = [NSString stringWithFormat:@"%@_%@_s.png",fileName,timeStr];
+}
+
+-(void)setDataModelWithAudioPath:(NSString * _Nonnull)AudioPath
+                            type:(CPDataModelUpload_Type)type
+                        fileName:(NSString * _Nonnull)fileName{
+    self.type = type;
+    self.audio_path = AudioPath;
+    self.audio_data = [NSData dataWithContentsOfFile:AudioPath];
+    NSString *timeStr = CPDataModelTimeIntervalSince1970;
+    self.audio_fileName = [NSString stringWithFormat:@"%@_%@.amr",fileName,timeStr];
+    self.audio_name = [NSString stringWithFormat:@"%@_%@",fileName,timeStr];
+}
+
+-(UIImage *)videoToImg:(NSURL *)videoUrl{
+    //截取图片
+    AVURLAsset *urlAsset = [[AVURLAsset alloc] initWithURL:videoUrl options:nil];
+    AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc] initWithAsset:urlAsset];
+    imageGenerator.appliesPreferredTrackTransform = YES;    // 截图的时候调整到正确的方向
+    CMTime time = CMTimeMakeWithSeconds(0.1, 700);   // 1.0为截取视频1.0秒处的图片，30为每秒30帧
+    CGImageRef cgImage = [imageGenerator copyCGImageAtTime:time actualTime:nil error:nil];
+    UIImage *image = [UIImage imageWithCGImage:cgImage];
+    
+    return image;
 }
 
 @end
